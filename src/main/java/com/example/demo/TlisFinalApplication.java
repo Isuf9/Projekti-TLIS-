ebcf3a61c1867a55064e6cc12490cd701bcd4cca
isufb <isufbajraktari5@gmail.com>
2020-01-16 14:09:37 +0100

    Krijimi i MVT

---
+++ src/main/java/com/example/demo/TlisFinalApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlisFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlisFinalApplication.class, args);
+
+
+    }
+
+}
